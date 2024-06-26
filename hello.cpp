6de721b020fdbec0bbdfe6c5445c1c1d60bb6253
Anubha25 <anubhav.ty11@gmail.com>
2024-06-27 00:16:52 +0530

    hello world added with heap

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 int main()
 {
     cout<<"Hello world";
+    priority_queue<pair<int,int> >maxheap;
+    maxheap.push({1,1});
+    maxheap.push({2,1});
+    maxheap.push({3,1});
+    //cout<<maxhea
 }
