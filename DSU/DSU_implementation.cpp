ef841d67cbafcc6ac8da3d34ace7687c88ad93ba
Anubha25 <anubhav.ty11@gmail.com>
2024-09-02 20:52:45 +0530

    DISJOINT_SET

--- DSU/DSU_implementation.cpp
+++ DSU/DSU_implementation.cpp
@@ -3,13 +3,15 @@
 
 #include<iostream>
 #include<vector>
+#include<list>
 using namespace std;
-class disjointset
+vector<list<int> >graph;
+class disjoint
 {
 vector<int> parent;
 vector<int> sizee;
 public:
-disjointset(int n)
+disjoint(int n)
 {
     parent.resize(n+1);
     sizee.resize(n+1);
@@ -24,7 +26,8 @@ int find(int u)
   return parent[u]=find(parent[u]);
 }
 bool Union(int u, int v)
-{
+{   if(u==v)
+     return true;
     int parent_u = find(u); //1
     int parent_v = find(v); //2
     if(parent_u==parent_v)
@@ -44,3 +47,50 @@ bool Union(int u, int v)
     return true;
 }
 };
+void addedge(int src, int dest,bool un_dir=true)
+{ if (src>=graph.size()-1 or dest>=graph.size()-1 or src==dest)
+    return;
+
+    for(int i:graph[src])
+       if(i==dest)
+         return;
+  graph[src].push_back(dest);
+  if(un_dir)
+  graph[dest].push_back(src);
+}
+void display()
+{
+    for(int i =0;i<graph.size();i++)
+    {cout<<i<<" :";
+        for(auto k:graph[i])
+        cout<<k<<" ";
+      cout<<endl;}
+}
+int main()
+{
+int n;
+cout<<"Size of graph :";
+cin>>n;
+graph.resize(n+1);
+disjoint *set = new disjoint(n+1);
+cout<<"Press 1 to add edge, 2 to display :";
+cin>>n;
+while(n!=0)
+{ int src,dest;
+if(n==1)
+{   cout<<"src";
+    cin>>src;
+    cout<<"dest";
+    cin>>dest;
+    addedge(src,dest,true);
+    if(!set->Union(src,dest))
+        cout<<"\n CYCLE EXIST AT "<<src<<" "<<dest<<endl;
+}
+else if(n==2)
+  display();
+
+cout<<"Press 1 to add edge 2 to display:";
+cin>>n;
+}
+}
+
